c8950e0f597d397cfc593440a5f5a18af992eab2
keeruthika <keeruthikaece@gmail.com>
2021-05-20 09:35:43 +0530

    formatted

--- src/app/registration-success/registration-success.component.ts
+++ src/app/registration-success/registration-success.component.ts
@@ -1,27 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { RegistrationService } from '../registration/registration.service';
+import { Component, OnInit } from "@angular/core";
+import { RegistrationService } from "../registration/registration.service";
 
 @Component({
-  selector: 'app-registration-success',
-  templateUrl: './registration-success.component.html',
-  styleUrls: ['./registration-success.component.less']
+  selector: "app-registration-success",
+  templateUrl: "./registration-success.component.html",
+  styleUrls: ["./registration-success.component.less"],
 })
 export class RegistrationSuccessComponent implements OnInit {
- username:string;
- password:string;
- mobile:number;
- address:string;
- emailid:string;
+  username: string;
+  password: string;
+  mobile: number;
+  address: string;
+  emailid: string;
 
-  constructor( private registrationService: RegistrationService ) { }
+  constructor(private registrationService: RegistrationService) {}
 
   ngOnInit() {
-  this.username=this.registrationService.registrationdto.userName;
-    this.password=this.registrationService.registrationdto.password;
-  this.mobile=this.registrationService.registrationdto.mobile;
-  this.address=this.registrationService.registrationdto.address;
-  this.emailid=this.registrationService.registrationdto.emailId;
-
+    this.username = this.registrationService.registrationdto.userName;
+    this.password = this.registrationService.registrationdto.password;
+    this.mobile = this.registrationService.registrationdto.mobile;
+    this.address = this.registrationService.registrationdto.address;
+    this.emailid = this.registrationService.registrationdto.emailId;
   }
-
 }
--- src/app/registration/registration.service.ts
+++ src/app/registration/registration.service.ts
@@ -1,20 +1,23 @@
-import { Injectable } from '@angular/core';
-import { RegistrationDTO } from '../dto/registration.dto';
-import { ApiService } from '../apiservice/api.service';
+import { Injectable } from "@angular/core";
+import { RegistrationDTO } from "../dto/registration.dto";
+import { ApiService } from "../apiservice/api.service";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-export class RegistrationService  {
-registrationdto:RegistrationDTO=new RegistrationDTO();
-//abc:string;
-//cc:number;
-//dd:[];
-  constructor(private apiService:ApiService) { }
+export class RegistrationService {
+  registrationdto: RegistrationDTO = new RegistrationDTO();
+  //abc:string;
+  //cc:number;
+  //dd:[];
+  constructor(private apiService: ApiService) {}
 
   saveData(abc) {
- // const abc=new RegistrationDTO();
- // abc.firstName="Inika";
-  //abc.lastName="Kisore";
-   return this.apiService.postData("http://15.207.6.226:8080/registerUser",abc);
+    // const abc=new RegistrationDTO();
+    // abc.firstName="Inika";
+    //abc.lastName="Kisore";
+    return this.apiService.postData(
+      "http://15.207.6.226:8080/registerUser",
+      abc
+    );
   }
 }
